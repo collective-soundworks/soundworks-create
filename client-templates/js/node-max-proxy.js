028a48a1933000a0e8edd31586b6335567852126
b-ma <benjamin.matuszewski@gmail.com>
2026-02-03 15:37:34 +0100

    updated templates

--- client-templates/js/node-max-proxy.js
+++
@@ -1,5 +0,0 @@
-// change process.cwd() to soundworks application root
-process.chdir('${d.relCwd}');
-console.log('node running in:', process.cwd());
-// import the client source file
-import('${d.relClientPathname}');
